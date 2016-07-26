2a74fca829cf05a5deaa591d99b8e4e09e7bf6df
Valeriya Slovikovskaya <valeriya.slovikovskaya@gruppometa.it>
2016-07-26 08:49:51 +0200

    Data for opera static graph: data cleaning

---
+++ docker-python/opera/clean_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+Transforms edges of multi graph into edges of edge wighted graph
+"""
+#  Valeriya Slovikovskaya <vslovik@gmail.com>
+
+
+def parse(input_file, output_file):
+    fh = open(output_file, "w")
+    with open(input_file, 'r') as lines:
+        for line in lines:
+            cleaned = line.replace(' Musica di Giacomo Puccini', '')\
+                .replace(' Opera in cinque atti, libretto di Luc Bondy, dal dramma omonimo di Arthur Schnitzler.', '')\
+                .replace("Melodramma in tre atti su  \\t\\t\\t\\t\\t\\tlibretto di Francesco Maria Piave", '')\
+                .replace('Tragedia giapponese in tre atti su Libretto di Luigi Illica e Giuseppe Giacosa','')
+            fh.write(cleaned)
+    fh.close()
+
+parse('/home/valerya/project/ars/ARS/docker-python/data/singer_event/singer_event_till2010.csv', '/home/valerya/project/ars/ARS/docker-python/data/singer_event/singer_event_till2010_cleaned.csv')
