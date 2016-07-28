90447272185893a7683b9143a915eb18e0467efe
Valeriya Slovikovskaya <vslovik@gmail.com>
2016-07-29 00:17:59 +0200

    Credits parsed

--- docker-python/opera/collector/parser.py
+++ docker-python/opera/collector/parser.py
@@ -4,6 +4,7 @@
 Collects opera performance data
 published on GBOPERA site
 """
+import re
 import os
 
 """www.gbopera.it crawling"""
@@ -12,13 +13,22 @@ __author__ = "Valeriya Slovikovskaya <vslovik@gmail.com>"
 __version__ = "0.1"
 __package__ = "collector"
 
+
 class Parser(object):
 
     PAGES_DIR = '/data/pages'
     HEADLINE_FILE = '/data/headline.txt'
 
-    @staticmethod
-    def parse():
+    ROLE_LINE_PATTERN = '<em>([^<>]+?)</em>([A-Z\s\/]+[A-Z])<br />'
+    TITLE_PATTERN = '<br />[^<>]+<strong>([A-Z\s\/]+[A-Z])?</strong>'
+    CREDIT_LINE_PATTERNS = ['<br />([^<>]+)<strong>([a-zA-Z\s\/]+[a-zA-Z])?</strong>',
+                            '<br />([^<>]+)<b>([a-zA-Z\s\/]+[a-zA-Z])?</b>']
+    CREDIT_ROLES = ['Musica di', 'Direttore', 'Maestro del Coro', 'Regia', 'Maestro del coro', 'musica di']
+
+    def __init__(self):
+        self.roles = set([])
+
+    def parse(self):
         dir_name = os.getcwd() + '/../../' + Parser.PAGES_DIR
         count = 0
         headlines = []
@@ -29,10 +39,9 @@ class Parser(object):
                 count += 1
                 fh = open(file_path, "r")
                 content = fh.read()
-                headline = Parser.parse_page(content, year, month)
+                headline = self.parse_page(content, year, month)
                 fh.close()
                 headlines.append(headline)
-                break
 
         headline_file = os.getcwd() + '/../../' + Parser.HEADLINE_FILE
         fh = open(headline_file, "w")
@@ -40,11 +49,9 @@ class Parser(object):
         fh.close()
         print(count)
 
-    @staticmethod
-    def parse_page(content, year, month):
+    def parse_page(self, content, year, month):
         headline = Parser.parse_headline(content, year, month)
-        entry = Parser.parse_credits(content)
-        print(entry)
+        entry = self.parse_credits(content, year)
         return headline
 
     @staticmethod
@@ -53,7 +60,7 @@ class Parser(object):
         pos = content.index(left_selector)
         content = content[pos + len(left_selector):]
         pos = content.index(right_selector)
-        headline = year + '|' + month  + '|' +  content[:pos].\
+        headline = year + '|' + month + '|' + content[:pos].\
             replace('<h1 class="entry-title" itemprop="name">', '').\
             replace('</h1>', '').\
             replace('&#8220;', '"').\
@@ -62,8 +69,7 @@ class Parser(object):
 
         return headline
 
-    @staticmethod
-    def parse_credits(content):
+    def parse_credits(self, content, year):
         left_selector, right_selector = '<div class="entry-content" itemprop="articleBody" style="color: #363636">',\
                                         '</div><div class="clear"></div>'
         pos = content.index(left_selector)
@@ -71,6 +77,23 @@ class Parser(object):
         pos = content.index(right_selector)
         entry = content[:pos]
 
+        pattern = re.compile(Parser.ROLE_LINE_PATTERN)
+        match = pattern.findall(entry)
+        for (role, name) in match:
+            print(role.strip() + '|' + name.strip() + '\n')
+
+        matches = []
+        for item in Parser.CREDIT_LINE_PATTERNS:
+            pattern = re.compile(item)
+            matches.append(pattern.findall(entry))
+
+        for match in matches:
+            for (role, name) in match:
+                r = role.replace('&nbsp;', ' ').strip()
+                if r in Parser.CREDIT_ROLES:
+                    print(r + '|' + name)
+
         return entry
 
-Parser.parse()
+
+Parser().parse()
