33bcad153e14610c9c8f2d5892e6e06bc33e8073
Valeriya Slovikovskaya <vslovik@gmail.com>
2016-08-16 00:59:48 +0200

    Threshold model for BA and ER graphs

--- threshold_model.py
+++ threshold_model.py
@@ -29,7 +29,7 @@ class ThresholdModel(object):
 
     RESULT_DIR = '/data/threshold_model/'
 
-    def __init__(self, graph, threshold=0.5, seed_size=200):
+    def __init__(self, graph, threshold=0.5, seed_size=200, seed_nodes=None):
         if not graph.size():
             raise Exception('Invalid graph')
         if threshold > 1:
@@ -40,7 +40,10 @@ class ThresholdModel(object):
         self.G = graph
         self.threshold = threshold
         self.seed_size = seed_size
-        self.seed = []
+        if seed_nodes:
+            self.seed = seed_nodes
+        else:
+            self.seed = []
         self.marked = dict()
         self.pq = PriorityQueue()
 
@@ -53,21 +56,26 @@ class ThresholdModel(object):
         giant = components.pop(0)
         return nx.center(giant)
 
-    def get_hubs(self):
-        degree = sorted(nx.degree(self.G).items(), key=lambda x: x[1], reverse=True)
+    @staticmethod
+    def get_hubs(graph, seed_size):
+        degree = sorted(nx.degree(graph).items(), key=lambda x: x[1], reverse=True)
         nodes = []
-        for (node, degree) in degree[0:self.seed_size]:
+        for (node, degree) in degree[0:seed_size]:
             nodes.append(node)
         return nodes
 
-    def get_center(self):
-        bt = nx.betweenness_centrality(self.G)
+    @staticmethod
+    def get_center_ego(graph):
+        bt = nx.betweenness_centrality(graph)
+        print(bt)
         for (node, betweenness) in sorted(bt.items(), key=lambda x: x[1], reverse=True):
-            nodes = nx.ego_graph(self.G, node).nodes()
+            nodes = nx.ego_graph(graph, node).nodes()
+            print(nodes)
             return nodes
 
-    def get_centers(self):
-        bt = nx.betweenness_centrality(self.G)
+    @staticmethod
+    def get_centers(graph):
+        bt = nx.betweenness_centrality(graph)
         centers = []
         i = 0
         for (node, betweenness) in sorted(bt.items(), key=lambda x: x[1], reverse=True):
@@ -85,11 +93,9 @@ class ThresholdModel(object):
         return seed
 
     def set_seed(self):
-        seed = self.get_centers()
-        # seed = self.get_center()
-        #seed = self.get_random_seed()
-        #seed = self.get_hubs()
-        self.seed = list(seed)
+        if not len(self.seed):
+            seed = self.get_random_seed()
+            self.seed = list(seed)
         for i in xrange(len(self.seed)):
             self.marked[self.seed[i]] = 1
         for i in xrange(len(self.seed)):
@@ -174,21 +180,24 @@ def get_opera_graph():
 thresholds = [float(i)/100. for i in range(100)]
 
 graph = get_opera_graph()
+seed = ThresholdModel.get_hubs(graph, 500)
 o_sizes = []
 for i in xrange(len(thresholds)):
-    o_sizes.append(ThresholdModel(graph, thresholds[i]).spread())
+    o_sizes.append(ThresholdModel(graph, thresholds[i], 500, seed).spread())
 print(o_sizes)
 
-graph = nx.barabasi_albert_graph(4604, 10)
-ba_sizes = []
-for i in xrange(len(thresholds)):
-    ba_sizes.append(ThresholdModel(graph, thresholds[i]).spread())
-print(ba_sizes)
+# graph = nx.barabasi_albert_graph(4604, 10)
+# seed = ThresholdModel.get_center_ego(graph)
+# ba_sizes = []
+# for i in xrange(len(thresholds)):
+#     ba_sizes.append(ThresholdModel(graph, thresholds[i], 200, ThresholdModel.get_center_ego(graph)).spread())
+# print(ba_sizes)
+#
+# graph = nx.erdos_renyi_graph(4604, 0.005)
+# seed = ThresholdModel.get_center_ego(graph)
+# er_sizes = []
+# for i in xrange(len(thresholds)):
+#     er_sizes.append(ThresholdModel(graph, thresholds[i], 200, ThresholdModel.get_center_ego(graph)).spread())
+# print(er_sizes)
 
-graph = nx.erdos_renyi_graph(4604, 0.005)
-er_sizes = []
-for i in xrange(len(thresholds)):
-    er_sizes.append(ThresholdModel(graph, thresholds[i]).spread())
-print(er_sizes)
-
-ThresholdModel.plot_spread_size_distribution(thresholds, o_sizes, ba_sizes, er_sizes)
+ThresholdModel.plot_spread_size_distribution(thresholds, o_sizes)
